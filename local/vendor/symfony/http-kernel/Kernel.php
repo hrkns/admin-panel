843aad414252f40040a5993b663e2516237c3d27
Jose Rosendo <jose.l.rosendo.s@gmail.com>
2016-10-10 16:52:58 -0400

    composer update

--- local/vendor/symfony/http-kernel/Kernel.php
+++ local/vendor/symfony/http-kernel/Kernel.php
@@ -58,11 +58,11 @@ abstract class Kernel implements KernelInterface, TerminableInterface
     protected $startTime;
     protected $loadClassCache;
 
-    const VERSION = '2.7.17';
-    const VERSION_ID = 20717;
+    const VERSION = '2.7.19';
+    const VERSION_ID = 20719;
     const MAJOR_VERSION = 2;
     const MINOR_VERSION = 7;
-    const RELEASE_VERSION = 17;
+    const RELEASE_VERSION = 19;
     const EXTRA_VERSION = '';
 
     const END_OF_MAINTENANCE = '05/2018';
